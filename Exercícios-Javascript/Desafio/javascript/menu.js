bcad0a3bb07be97707283ef12f10f06a8b7a6766
Alesamsil <alesamsil@gmail.com>
2021-09-06 19:35:17 -0300

    Desafio

--- Exercícios-Javascript/Desafio/javascript/menu.js
+++ Exercícios-Javascript/Desafio/javascript/menu.js
@@ -3,10 +3,19 @@
     var $html = document.querySelector('html');
     var $btn = document.querySelector('.header-nav__hamburgger');
     
+    var ariaControl = $btn.getAttribute('aria-controls');
+        
+    var $menu = document.getElementById(ariaControl);
+    
     $html.classList.remove('no-js');
     $html.classList.add('js');
     
     $btn.addEventListener('click', function(){
-        $html.classList.toggle('menu-opened');
+        $html.classList.toggle('menu-opened');        
+        
+        var ariaExpanded = this.getAttribute('aria-expanded') === "true";
+        
+        $btn.setAttribute('aria-expanded', !ariaExpanded);       $menu.setAttribute('aria-expanded', !ariaExpanded);       
+        
     })
 })()
